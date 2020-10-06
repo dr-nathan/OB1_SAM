6ba0b13f8bf2aba985b78a22b1f9c3d552938636
nsl620 <nsl620@pfgbl001.labs.vu.nl>
2020-10-06 15:50:54 +0200

    rerun EA with new decay param

--- parameters.py
+++ parameters.py
@@ -47,7 +47,7 @@ print("-------------------")
 
 
 ## Monoweight = 1
-decay = -0.053
+decay = -0.2 #-0.053
 bigram_to_word_excitation = 2.18 # inp. divded by #ngrams, so this param estimates excit per word [diff from paper]
 bigram_to_word_inhibition = -0.55
 word_inhibition = -0.011 # -0.002
